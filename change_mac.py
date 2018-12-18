97b71359934bfd682dfce798186f165b091ab2f6
root <root@localhost.localdomain>
2018-12-19 00:21:27 +0300

    initial Commit

---
+++ change_mac.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python
+
+
+import subprocess
+
+
+def mac_address_backup(interface, file="backup.txt"):
+    cmd_backup = "ip link show {0} | awk '{1}' ".format(interface,"/ether/ {print $2}")
+    print(cmd_backup)
+    mac_address = subprocess.check_output(cmd_backup, shell=True)
+    with open(file, 'a') as file_handler:
+        file_handler.write(mac_address.decode("utf-8"))
+
+
+def mac_changer(mac_adress, interface):
+    cmd_up = "ifconfig {0} up ".format(interface)
+    cmd_change = "ifconfig {0} hw ether {1} ".format(interface, mac_adress)
+    cmd_down = "ifconfig {0} down ".format(interface)
+
+    subprocess.call(cmd_down, shell=True)
+    subprocess.call(cmd_change, shell=True)
+    subprocess.call(cmd_up, shell=True)
+
+
+if __name__ == "__main__":
+    print(subprocess.call("ifconfig", shell=True))
+    mac_address_backup("eth0")
+    mac_changer("00:11:22:33:44:55", "eth0")
+    print(subprocess.call("ifconfig", shell=True))
